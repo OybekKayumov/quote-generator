9e19ce5ad38339b48bbd466945bdb96efa2e36d7
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-24 11:05:56 +0500

    Add random quote

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // get quotes from api
 let apiQuotes = [];
 
 // show new quote
 function newQuote() {
   // pick a random quote from apiQuotes array
-  // const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];  // 1643
-  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];  
+  // const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];  
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];  
+  // 1643 quotes
   console.log(': ', quote);
+
+  authorText.textContent = quote.author;
+  quoteText.textContent = quote.text;
 }
-/*
+
 async function getQuotes() {
   const apiUrl = 'https://type.fit/api/quotes';
 
@@ -26,5 +36,5 @@ async function getQuotes() {
 
 // on load
 getQuotes();
-*/
-newQuote();
+
+// newQuote(); // local
