4c7a8b8dcdd446dd0061122806fa352d8cabcd82
Oybek Kayumov <oybek.kayumov@gmail.com>
2023-02-24 12:03:39 +0500

    Add loader

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // get quotes from api
 let apiQuotes = [];
 
+// show loader
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // show new quote
 function newQuote() {
+  loading();
+
   // pick a random quote from apiQuotes array
   // const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];  
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];  
@@ -27,10 +42,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove('long-quote')
   }
+
   quoteText.textContent = quote.text;
+  complete();
 }
 
 async function getQuotes() {
+  loading();
+
   const apiUrl = 'https://type.fit/api/quotes';
 
   try {
@@ -60,3 +79,5 @@ twitterBtn.addEventListener('click', tweetQuote);
 getQuotes();
 
 // newQuote(); // local
+
+// loading();
